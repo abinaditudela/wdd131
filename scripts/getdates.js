213656520a882d069842eace788364a5bb5ed13d
abinaditudela <abinadi.tc@gmail.com>
2024-06-30 01:18:45 -0400

    getlasmodified-js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,18 +2,13 @@
 const lastModified = document.querySelector("#lastModified");
 const currentyear = document.querySelector("#currentyear");
 
-
 // use the date object
 const today = new Date();
 
-lastModified.innerHTML = `Last Modified <span class="highli">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "full"
-	}
-).format(today)}</span>`;
+lastModified.innerHTML = `Last Modified <span class="highli">${document.lastModified}</span>`;
 
 console.log(today.getFullYear());
 console.log(today);
+console.log(document.lastModified)
 
 currentyear.innerHTML = `&copy; <span class="highli">${today.getFullYear()}</span>`;
